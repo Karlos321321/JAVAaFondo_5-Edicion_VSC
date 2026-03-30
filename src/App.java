184e658c64ebd35e6d8212574930d695966880fb
Karlos321321 <162877887+Karlos321321@users.noreply.github.com>
2026-03-30 16:29:11 +0200

    commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+
+//-------------------
+//Programa Hola Mundo
+//-------------------
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola, vamos adelante, nunca hacia atras !!!!!");
+        System.out.println("\n\\Hola, vamos \"adelante\", \n\'nunca\' \"hacia atras\" \\ !!!!\n");
     }
 }
